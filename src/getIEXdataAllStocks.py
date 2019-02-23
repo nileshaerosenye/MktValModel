abcf68165d7d7c49df7a3d2f8361c73391bf00da
Nilesh Mune <nileshmune@rrcs-64-183-222-2.sw.biz.rr.com>
2019-02-23 14:47:42 -0600

    IEX API added to get mktcap for all list

---
+++ src/getIEXdataAllStocks.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 11 23:00:21 2019
+
+@author: nileshmune
+This code scraps the narket cap value from the Zacks website, for a given list of stocks.
+"""
+
+# DataFrame proessing
+import pandas as pd
+import requests
+
+
+symbols = pd.read_csv("../data/combined.csv")
+print(symbols)
+
+baseURL="https://api.iextrading.com/1.0"
+
+# Get daily chart details every minute trading : https://api.iextrading.com/1.0/stock/aapl/chart/1d
+# Get company details: industry, ceo: https://api.iextrading.com/1.0/stock/aapl/company
+# Earnings: https://api.iextrading.com/1.0/stock/aapl/earnings
+# Dividends: https://api.iextrading.com/1.0//stock/aapl/dividends/6m
+# Pulls income statement, balance sheet, and cash flow data from the four most recent reported quarters.
+# https://api.iextrading.com/1.0//stock/aapl/financials
+# IPO Calendar: https://api.iextrading.com/1.0/stock/market/upcoming-ipos
+# Important Key Stats containing all I need: https://api.iextrading.com/1.0//stock/aapl/stats
+
+def processSymbol( sym ):
+    request = requests.get("https://api.iextrading.com/1.0//stock/" + sym + "/stats")
+    if ( request.ok ):
+        print(sym, " : Ok")
+        
+        jsonRec = request.json()
+
+        marketcap=jsonRec['marketcap']
+        """        latestEPS=jsonRec['latestEPS']
+        returnOnEquity=jsonRec['returnOnEquity']
+        revenue=jsonRec['revenue']
+        grossProfit=jsonRec['grossProfit']
+        cash=jsonRec['cash']
+        debt=jsonRec['debt']
+        ttmEPS=jsonRec['ttmEPS']
+        revenuePerShare=jsonRec['revenuePerShare']
+        peRatioHigh=jsonRec['peRatioHigh']
+        peRatioLow=jsonRec['peRatioLow']
+        returnOnAssets=jsonRec['returnOnAssets']
+        returnOnCapital=jsonRec['returnOnCapital']
+        profitMargin=jsonRec['profitMargin']
+        priceToSales=jsonRec['priceToSales']
+        """             
+        
+        fLine=sym+","+str(marketcap)+"\n"
+        #print(fLine)
+        fCSV.write(fLine)
+        
+    else:
+        print(sym, ": Failed.")
+    
+    
+
+#request = requests.get()
+fCSV = open("../data/allMktCap.csv","w")
+fCSV.write("Symbol,marketcap\n")
+symbols['Symbol'].apply(lambda x: processSymbol(x) )
+fCSV.close()
+
